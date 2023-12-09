cb65a9660ac2b9cf6f51334195151198a37a05cd
DmitryR <dmytro.ryabchun@gmail.com>
2023-12-09 23:52:50 +0200

    🔨 Implemented dynamic form field generation for characters-playable-create

--- src/app/core/classes/shared-create/shared-create.ts
+++ src/app/core/classes/shared-create/shared-create.ts
@@ -1,13 +1,16 @@
 import { FormBuilder } from '@angular/forms';
 import { EBtnType } from "../../enums/btn-type.enum";
 import { EPage } from "../../enums/page.enum";
+import { IFormMetadata } from "../../../shared/interfaces/form.interface";
+import { EFormType } from "../../enums/form.enum";
 
 export abstract class SharedCreate<T> {
   public readonly btnType: EBtnType = EBtnType.Submit;
   public readonly viewLink: string = '../' + EPage.View;
-  protected readonly translateKey: string = 'page.characters.playable.create.';
+  public readonly fieldType: typeof EFormType = EFormType;
 
   public form!: T;
+  public metadata: IFormMetadata[] = [];
 
   protected constructor(protected formBuilder: FormBuilder) {}
 
--- src/app/modules/characters/services/characters-playable-metadata/characters-playable-metadata.service.ts
+++ src/app/modules/characters/services/characters-playable-metadata/characters-playable-metadata.service.ts
@@ -4,12 +4,14 @@ import { viewMetadata } from "../../data-structures/view.sctructure";
 import { editMetadata } from "../../data-structures/edit.structure";
 import { IFormMetadata } from "../../../../shared/interfaces/form.interface";
 import { detailsMetadata } from "../../data-structures/details.sctructure";
+import { createMetadata } from "../../data-structures/create.sctructure";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharactersPlayableMetadataService {
   private readonly viewMetadata: ITableMetadata[] = viewMetadata;
+  private readonly createMetadata: IFormMetadata[] = createMetadata;
   private readonly editMetadata: IFormMetadata[] = editMetadata;
   private readonly detailsMetadata: IFormMetadata[] = detailsMetadata;
 
@@ -19,8 +21,8 @@ export class CharactersPlayableMetadataService {
     return this.viewMetadata;
   }
 
-  public getCreate(): void {
-
+  public getCreate(): IFormMetadata[] {
+    return this.createMetadata;
   }
 
   public getEdit(): IFormMetadata[] {
