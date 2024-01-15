44cd07ee3620c4b4848b7c0a036d91be3600ba8f
DmitryR <dmytro.ryabchun@gmail.com>
2024-01-15 18:38:52 +0200

    🚧 Updated pages for weapon module

--- src/app/pages/main/weapons/weapons.component.ts
+++ src/app/pages/main/weapons/weapons.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'clt-weapons',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './weapons.component.html',
   styleUrl: './weapons.component.scss',
 })
