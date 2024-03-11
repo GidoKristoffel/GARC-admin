8972b48bd86167fa01a529eb6fb31f052e757734
DmitryR <dmytro.ryabchun@gmail.com>
2024-03-11 20:07:52 +0200

    ✨ Added form api interface for artifact leveling category module

---
+++ src/app/modules/artifact-leveling-category/interfaces/api.interface.ts
@@ -0,0 +1,27 @@
+import { HttpStatusCode } from "@angular/common/http";
+import { IArtifactLevelingCategory } from "./common.inteface";
+
+export interface IArtifactLevelingCategoryCreateResponse {
+  artifactLevelingCategory: IArtifactLevelingCategory;
+  status: HttpStatusCode;
+}
+
+export interface IArtifactLevelingCategoryViewResponse {
+  artifactLevelingCategorys: IArtifactLevelingCategory[];
+  status: HttpStatusCode;
+}
+
+export interface IArtifactLevelingCategoryDetailsResponse {
+  artifactLevelingCategory: IArtifactLevelingCategory;
+  status: HttpStatusCode;
+}
+
+export interface IArtifactLevelingCategoryEditResponse {
+  artifactLevelingCategory: IArtifactLevelingCategory;
+  status: HttpStatusCode;
+}
+
+export interface IArtifactLevelingCategoryDeleteResponse {
+  artifactLevelingCategory: { id: string } | null;
+  status: HttpStatusCode;
+}
