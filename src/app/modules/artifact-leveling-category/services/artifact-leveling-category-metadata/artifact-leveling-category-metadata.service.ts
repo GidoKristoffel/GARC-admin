87c24907dd649d9aff9b455608fee90224a4268c
DmitryR <dmytro.ryabchun@gmail.com>
2024-03-14 19:17:37 +0200

    ✨ Added metadata service for artifact leveling category module

---
+++ src/app/modules/artifact-leveling-category/services/artifact-leveling-category-metadata/artifact-leveling-category-metadata.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { ITableMetadata } from "../../../../shared/interfaces/table.interface";
+import { IFormMetadata } from "../../../../shared/interfaces/form.interface";
+import { createMetadata } from "../../data-structures/create.sctructure";
+import { detailsMetadata } from "../../data-structures/details.sctructure";
+import { editMetadata } from "../../data-structures/edit.structure";
+import { viewMetadata } from "../../data-structures/view.sctructure";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArtifactLevelingCategoryMetadataService {
+  private readonly viewMetadata: ITableMetadata[] = viewMetadata;
+  private readonly createMetadata: IFormMetadata[] = createMetadata;
+  private readonly editMetadata: IFormMetadata[] = editMetadata;
+  private readonly detailsMetadata: IFormMetadata[] = detailsMetadata;
+
+  constructor() {}
+
+  public getView(): ITableMetadata[] {
+    return this.viewMetadata;
+  }
+
+  public getCreate(): IFormMetadata[] {
+    return this.createMetadata;
+  }
+
+  public getEdit(): IFormMetadata[] {
+    return this.editMetadata;
+  }
+
+  public getDetails(): IFormMetadata[] {
+    return this.detailsMetadata;
+  }
+}
