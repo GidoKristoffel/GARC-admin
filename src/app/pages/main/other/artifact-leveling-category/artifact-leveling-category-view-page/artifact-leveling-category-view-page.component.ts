9ed509f4a857c4f2d8f3cb834db33f9d6cfa185c
DmitryR <dmytro.ryabchun@gmail.com>
2024-03-29 20:09:10 +0200

    🚧 Routing improvement

--- src/app/pages/main/other/artifact-leveling-category/artifact-leveling-category-view-page/artifact-leveling-category-view-page.component.ts
+++ src/app/pages/main/other/artifact-leveling-category/artifact-leveling-category-view-page/artifact-leveling-category-view-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  ArtifactLevelingCategoryEditComponent
+} from "../artifact-leveling-category-edit/artifact-leveling-category-edit.component";
 
 @Component({
   selector: 'clt-artifact-leveling-category-view-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ArtifactLevelingCategoryEditComponent],
   templateUrl: './artifact-leveling-category-view-page.component.html',
   styleUrl: './artifact-leveling-category-view-page.component.scss'
 })
