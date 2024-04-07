e0c31d24ecb395b9ee0e01fb94a8268c38ac945e
DmitryR <dmytro.ryabchun@gmail.com>
2024-04-07 20:05:11 +0300

    🚧 Updated pages

--- src/app/pages/main/other/artifact-leveling-category/artifact-leveling-category-create-page/artifact-leveling-category-create-page.component.ts
+++ src/app/pages/main/other/artifact-leveling-category/artifact-leveling-category-create-page/artifact-leveling-category-create-page.component.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import {
   AscensionMaterialsCreateComponent
 } from "../../../../../modules/ascension-material/components/ascension-materials-create/ascension-materials-create.component";
+import {
+  ArtifactLevelingCategoryCreateComponent
+} from "../../../../../modules/artifact-leveling-category/components/artifact-leveling-category-create/artifact-leveling-category-create.component";
 
 @Component({
   selector: 'clt-artifact-leveling-category-create-page',
   standalone: true,
-  imports: [CommonModule, AscensionMaterialsCreateComponent],
+  imports: [CommonModule, AscensionMaterialsCreateComponent, ArtifactLevelingCategoryCreateComponent],
   templateUrl: './artifact-leveling-category-create-page.component.html',
   styleUrl: './artifact-leveling-category-create-page.component.scss'
 })
