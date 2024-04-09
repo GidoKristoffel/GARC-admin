f3bc535304b7688b3a6b4dc7066b07cd441154cf
DmitryR <dmytro.ryabchun@gmail.com>
2024-04-09 20:19:00 +0300

    🚧 Updated number field

--- src/app/shared/components/fields/number-field/number-field.component.ts
+++ src/app/shared/components/fields/number-field/number-field.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FieldLineDirective } from "../../../directives/field-line/field-line.directive";
+import { TranslateModule } from "@ngx-translate/core";
+import { NumberInputComponent } from "../../input/number-input/number-input.component";
 
 @Component({
   selector: 'clt-number-field',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FieldLineDirective, TranslateModule, NumberInputComponent],
   templateUrl: './number-field.component.html',
   styleUrl: './number-field.component.scss'
 })
 export class NumberFieldComponent {
-
+  @Input() label: string = '';
+  @Input() formField: string = '';
 }
